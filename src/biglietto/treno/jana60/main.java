b6052e96dacc8a903a62ffedfcee73962b67c7ff
OlivieroPinto <pintooliviero@gmail.com>
2022-06-17 15:26:07 +0200

    aggiunta del bonus

--- src/biglietto/treno/jana60/main.java
+++ src/biglietto/treno/jana60/main.java
@@ -6,19 +6,22 @@ public class main {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int eta, km;
+		int eta, km, numero = 1;
 		do {
-			System.out.println("Benvenuto alla biglietteria, inserisci la tua etá");
-			eta = Integer.parseInt(scan.nextLine());
-			if (eta < 1)
-				System.out.println("etá non valida");
-		} while (eta < 1);
+			do {
+				System.out.println("Benvenuto alla biglietteria, inserisci la tua etá");
+				eta = scan.nextInt();
+				if (eta < 1)
+					System.out.println("etá non valida");
+			} while (eta < 1);
 
-		System.out.println("Inserisci km da percorrere");
-		km = Integer.parseInt(scan.nextLine());
-		Biglietteria t = new Biglietteria(eta, km);
-		System.out.println(t.toString());
+			System.out.println("Inserisci km da percorrere");
+			km = scan.nextInt();
+			Biglietteria t = new Biglietteria(eta, km);
+			System.out.println(t.toString());
+			System.out.println("per continuare digita 1");
+			numero = scan.nextInt();
+		} while (numero == 1);
 		scan.close();
-
 	}
 }
